b10d7b46ead65763ec1201975f68d51acf87e5a2
DavidShaibu <davidshaibu44@gmail.com>
2023-08-23 22:36:57 +0100

    Display the Genres Images

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,19 @@ import NavBar from "./components/NavBar";
 function App() {
 
   return (
-  <Grid templateAreas={{
+  <Grid 
+    templateAreas={{
     base: `"nav" "main"`,
     lg: `"nav nav" "aside main"`
-  }}>
+  }}
+    templateColumns={{
+      base: "1fr",
+      lg: "200px 1fr"
+    }}
+  >
     <GridItem area="nav"><NavBar></NavBar></GridItem>
     <Show above="lg">
-      <GridItem area="aside">
+      <GridItem area="aside" paddingY="5px">
         <GenreList/>
       </GridItem>
     </Show>
