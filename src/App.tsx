33914690a1a8ecd2c41466cafa01cf6cb91ab4a6
DavidShaibu <davidshaibu44@gmail.com>
2023-08-24 00:54:21 +0100

    Filter games by platform

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,41 @@ import { useState } from "react";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import NavBar from "./components/NavBar";
+import PlatformSelector from "./components/PlatformSelector";
+import { Platform } from "./hooks/useGames";
 import { Genre } from "./hooks/useGenres";
-
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
 
   return (
-  <Grid 
-    templateAreas={{
-    base: `"nav" "main"`,
-    lg: `"nav nav" "aside main"`
-  }}
-    templateColumns={{
-      base: "1fr",
-      lg: "200px 1fr"
-    }}
-  >
-    <GridItem area="nav"><NavBar></NavBar></GridItem>
-    <Show above="lg">
-      <GridItem area="aside" paddingY="5px">
-        <GenreList selectedGenre={selectedGenre} onSelectGenre={(genre) => setSelectedGenre(genre)}/>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar></NavBar>
       </GridItem>
-    </Show>
-    <GridItem area="main"><GameGrid selectedGenre={selectedGenre}/></GridItem>
-  </Grid>)
+      <Show above="lg">
+        <GridItem area="aside" paddingY="5px">
+          <GenreList
+            selectedGenre={selectedGenre}
+            onSelectGenre={(genre) => setSelectedGenre(genre)}
+          />
+        </GridItem>
+      </Show>
+      <GridItem area="main">
+        <PlatformSelector selectedPlatform={selectedPlatform} onSelectPlatform={(platform) => setSelectedPlatform(platform)}/>
+        <GameGrid selectedGenre={selectedGenre} selectedPlatform={selectedPlatform}/>
+      </GridItem>
+    </Grid>
+  );
 }
-export default App
+export default App;
